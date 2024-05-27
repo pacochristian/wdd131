0738a56398d1740f6a4d6112f11f4b2baf029314
pacochristian <pacochristiangracekifoueto@gmail.com>
2024-05-27 21:27:02 +0100

    fa

---
+++ scripts/place.js
@@ -0,0 +1,13 @@
+// This block of code is going to diplay the current year
+
+const today = new Date();
+
+let year = today.getFullYear();
+
+document.getElementById("currentyear").innerHTML = year;
+
+// This block of code is going to get the Last Modified
+
+let text = document.lastModified;
+
+document.getElementById("lastModified").innerHTML = text;
